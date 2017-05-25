85e6646bf438136e7d5ee404072c98c5b48e0d67
David Stone <davidstone.nyc@gmail.com>
2017-05-25 18:12:51 +0000

    midi input enabled

--- client/app/main.js
+++ client/app/main.js
@@ -1,5 +1,6 @@
 
 import React from 'react'
+import WebMidi from "webmidi";
 import { render } from 'react-dom'
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
@@ -9,7 +10,7 @@ import reducers from './reducers/reducers.js';
 import mySaga from './sagas/sagas.js';
 import initialState from "./state.js";
 
-import { generateKeys, setPlayer } from "./actions/actions.js";
+import { generateKeys, setPlayer, pressKeySaga, releaseKeySaga } from "./actions/actions.js";
 import App from './app.js';
 
 
@@ -22,9 +23,17 @@ const AudioContext = window.AudioContext // Default
     || false; 
 
 let ac = new AudioContext;
-let store;
+let store, input;
 
-const persistedState = localStorage.getItem("reduxState") ? JSON.parse(localStorage.getItem("reduxState")) : initialState;
+let persistedState = localStorage.getItem("reduxState") ? JSON.parse(localStorage.getItem("reduxState")) : initialState;
+
+
+
+WebMidi.enable(function (err) {
+    console.log(WebMidi.inputs);
+    console.log(WebMidi.outputs);
+    input = WebMidi.inputs[0];
+});
 
 Soundfont.instrument(ac, 'acoustic_grand_piano')
 
@@ -34,6 +43,28 @@ Soundfont.instrument(ac, 'acoustic_grand_piano')
             sagaMiddleware.run(mySaga)
             store.dispatch(generateKeys(persistedState.gameState.size,persistedState.gameState.offset));
             store.dispatch(setPlayer(piano,ac));
+            
+            if (input) {
+            
+                input.addListener('noteon', "all",
+                    function (e) {
+                    console.log("Received 'noteon' message (" + e.note.number + ").");
+                    if (store.getState().navigation.currentPage === "/interface") {
+                        store.dispatch(pressKeySaga(parseInt(e.note.number), 20, 150));
+                        }
+                    }
+                );
+                
+                input.addListener('noteoff', "all",
+                    function (e) {
+                    console.log("Received 'noteoff' message (" + e.note.number + ").");
+                    if (store.getState().navigation.currentPage === "/interface") {
+                        store.dispatch(releaseKeySaga(parseInt(e.note.number)));
+                        }
+                    }
+                );
+            }
+            
             store.subscribe(() => {
                 console.log("saving state",store.getState());
                 localStorage.setItem("reduxState", JSON.stringify(store.getState()));
